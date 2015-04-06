d0507b6b2a074b301ee26d0ae6e75c23a7981a6a
Ehsan70 <a.ehsan70@gmail.com>
2015-04-06 16:47:14 -0700

    Added proper names for PL nodes

--- ShellScript/ProcessHandle.sh
+++ ShellScript/ProcessHandle.sh
@@ -2,18 +2,19 @@
 # This script is ran by the cronjob
 
 # This holds where the python script is stored
-DIR="PL/Cogs/all/kvstore"
+DIR="PL/Cogs/all/kvstore/app.py"
 # I used this just to test 
 #DIR_TEST=`readlink -f ../FakeProcess.py`
 DIR_TEST="/home/user/Documents/PyCharm/ProcessChecker/FakeProcess.py"
 # This is the name of process which we will have an eye on.
-PNAME="kvStore"
+#PNAME="kvStore"
+PNAME="python"
 
 # ID of the process with the above name 
 PID=`pgrep $PNAME`
 
 
-echo "Script is running"
+echo "[BASH] Script is running"
 
 if [ ! -z  "$PID" ]; then 
 	echo "[BASH] The pid for specified process $PNAME is $PID  "
