ab8118e7204ef1606d6404eaa9c4abc0aacd9dfe
Ehsan70 <a.ehsan70@gmail.com>
2015-04-07 06:13:09 -0700

    Added a TODO, detecting the fodler need to be smarter.

--- ShellScript/DeployScripts/downloader.bash
+++ ShellScript/DeployScripts/downloader.bash
@@ -3,7 +3,7 @@
 # -y would ignore all the prompts 
 # Source: http://unix.stackexchange.com/questions/84310/how-do-i-force-yum-to-install-without-prompting-the-user-using-bash 
 yum -y install git
-
+#[TODO] Need to make it smartter so that in can detect if the folder already exists 
 mkdir /home/ubc_eece411_3/cronjob
 
 cd /home/ubc_eece411_3/cronjob
