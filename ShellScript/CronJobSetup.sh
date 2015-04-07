d109f9cdec9bc5b49ad04ad839b5ff7dc1c58bdb
Ehsan70 <a.ehsan70@gmail.com>
2015-04-06 19:03:34 -0700

    Added the command which starts the crond service on the node

--- ShellScript/CronJobSetup.sh
+++ ShellScript/CronJobSetup.sh
@@ -4,7 +4,7 @@
 PH_DIR=`readlink -f ./ProcessHandle.sh`
 
 # This will run every 2 mins
-CRON_CMD="*/2 * * * * $PH_DIR >> /DHTlog.txt 2>&1"
+CRON_CMD="*/2 * * * * $PH_DIR >> /home/ubc_eece411_3/PL/Cogs/all/DHTlog.txt 2>&1"
 
 #set the environment variable prior to the cron job running.
 #source: http://unix.stackexchange.com/questions/94456/how-to-change-cron-shell-sh-to-bash 
@@ -16,5 +16,6 @@ echo "$CRON_CMD" >> CronJobs.txt
 # Load the CronJobs.txt file into the crontab
 sudo crontab CronJobs.txt
 
-
+# start the crond service
+sudo /sbin/service crond start
 
